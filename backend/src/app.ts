1d2d515676717d7e82a8da213e88e7ad6858718d
pittu777 <pittuprasanth98@gmail.com>
2026-02-28 16:45:05 +0530

    (feat): "added frontend to create workspace"

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/work", workSPaceRouter);
+app.use("/api/v1/workspaces", workSPaceRouter);
 
 app.use(globelErrorHandler);
 
